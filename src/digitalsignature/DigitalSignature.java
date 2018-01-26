e06b6b22d6b9059f6cbdc9b4538cf4b77c7dcaf8
Luis Rodrigues <Luis Rodrigues@192.168.60.63>
2018-01-26 17:51:30 +0000

    commit 3.1 - reorganizei a classe com os métodos. 			Fiz a funcao para calcular os Digests. Acho que nao esta a guardar em Base64

--- src/digitalsignature/DigitalSignature.java
+++ src/digitalsignature/DigitalSignature.java
@@ -18,41 +18,18 @@ public class DigitalSignature {
     public static void main(String[] args) throws Exception{
        //System.out.println( Paths.get(".").toAbsolutePath().normalize().toString()); ** get working directory path
        byte[] file = Files.readAllBytes(new File ("test.txt").toPath());
-       printFileContent("test.txt");
+       CipherMethods.printFileContent("test.txt");
        
        //instanciar o gerador de chaves e gerar uma chave
        KeyGenerator keygen = KeyGenerator.getInstance("AES");
        SecretKey key = keygen.generateKey();
        
-       // instanciar as ciphers
-       Cipher c_enc = Cipher.getInstance("AES");
-       c_enc.init(Cipher.ENCRYPT_MODE, key);
        
-       Cipher c_dec = Cipher.getInstance("AES");
-       c_dec.init(Cipher.DECRYPT_MODE, key);
+       CipherMethods.cipherFile("test.txt", key);
+       CipherMethods.decipherFile("enc.txt", key);
        
+       CipherMethods.digestFile("test.txt");
        
-       //encripta o ficheiro
-       byte[] fichEncriptado = c_enc.doFinal(file);
-       Files.write(new File("enc.txt").toPath(),fichEncriptado);
-       printFileContent("enc.txt");
-       
-       //decifrar o ficheiro
-       // fazer novamente a leitura do ficheiro a decifrar e decifrar
-       file = Files.readAllBytes(new File ("enc.txt").toPath());
-       byte[] fichDecifrado = c_dec.doFinal(file);
-       
-       Files.write(new File("dec.txt").toPath(),fichDecifrado);
-       printFileContent("dec.txt");
-       
-       
-       
-    }
-    
-    public static void printFileContent(String fileName) throws IOException{
-       byte[] file = Files.readAllBytes(new File (fileName).toPath());
-       String s = new String (file);
-       System.out.println(s);
     }
     
 }
